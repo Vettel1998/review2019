caa53a3d113e6661638c96d19728ac58fb0a0946
vettelx <1105612690@qq.com>
2019-10-05 21:56:26 +0800

    add from ubuntu

---
+++ review/src/main/java/designpattern/principles/singleresponsibility/SingleResponsibility.java
@@ -0,0 +1,14 @@
+package designpattern.principles.singleresponsibility;
+
+public class SingleResponsibility {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Vehicle{
+    public void run(String vehicle){
+
+    }
+}
