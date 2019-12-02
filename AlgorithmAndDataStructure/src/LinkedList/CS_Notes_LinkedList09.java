d9d73ed52e62a92965cad7abc94c81526ffcd457
vettelx <161250168@smail.nju.edu.cn>
2019-12-02 18:15:11 +0800

    add

--- AlgorithmAndDataStructure/src/LinkedList/CS_Notes_LinkedList09.java
+++ AlgorithmAndDataStructure/src/LinkedList/CS_Notes_LinkedList09.java
@@ -5,4 +5,32 @@ package LinkedList;
  * Leetcode725. Split Linked List in Parts(Medium)
  */
 public class CS_Notes_LinkedList09 {
+    public ListNode[] splitListToParts(ListNode root, int k) {
+        ListNode[] res = new ListNode[k];
+        int len = 0;
+        ListNode cur = root;
+        while (cur!=null){
+            len++;
+            cur = cur.next;
+        }
+
+        int width = len/k;
+        int rem = len%k;
+
+        cur = root;
+        for (int i = 0; i < k; i++) {
+            ListNode head = cur;
+            for (int j = 0; j <(width + (i < rem ? 1 : 0)-1); j++) {
+                if (cur!=null) cur = cur.next;
+            }
+            if (cur!=null){
+                ListNode prev = cur;
+                cur = cur.next;
+                prev.next = null;
+
+            }
+            res[i] = head;
+        }
+        return res;
+    }
 }
--- AlgorithmAndDataStructure/src/LinkedList/CS_Notes_LinkedList10.java
+++ AlgorithmAndDataStructure/src/LinkedList/CS_Notes_LinkedList10.java
@@ -5,4 +5,19 @@ package LinkedList;
  * Leetcode328. Odd Even Linked List (Medium)
  */
 public class CS_Notes_LinkedList10 {
+    public ListNode oddEvenList(ListNode head) {
+        if (head==null||head.next==null) return head;
+        ListNode odd = head;
+        ListNode evenHead = head.next;
+        ListNode even = head.next;
+        while (even!=null&&even.next!=null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
 }
