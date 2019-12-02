047bb3643ffdcbff112a861ebbff5a12a281f0bb
vettelx <161250168@smail.nju.edu.cn>
2019-12-02 09:56:29 +0800

    add

--- AlgorithmAndDataStructure/src/LinkedList/CS_Notes_LinkedList08.java
+++ AlgorithmAndDataStructure/src/LinkedList/CS_Notes_LinkedList08.java
@@ -6,4 +6,56 @@ package LinkedList;
  * Leetcode234. Palindrome Linked List (Easy)
  */
 public class CS_Notes_LinkedList08 {
+
+    //快慢指针 将前半段翻转
+    public static boolean isPalindrome(ListNode head) {
+        if (head==null||head.next==null) return true;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode pre = null;
+        ListNode next = head;
+        while (fast!=null&&fast.next!=null){
+            fast = fast.next.next;
+            next = slow.next;
+            slow.next = pre;
+            pre = slow;
+            slow = next;
+        }
+
+        if (fast!=null){
+            slow = slow.next;
+        }
+        /*System.out.println("pre:"+pre.val);
+        System.out.println("pre.next:"+pre.next.val);
+        System.out.println("slow:"+slow.val);
+        System.out.println("slow.next:"+slow.next.val);
+        System.out.println("next:"+next.val);*/
+
+        while (slow!=null&&pre!=null){
+            if (slow.val!=pre.val)
+                return false;
+            pre = pre.next;
+            slow = slow.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode head1 = head;
+        head.next = new ListNode(2);
+        head = head.next;
+        head.next = new ListNode(3);
+        head = head.next;
+        head.next = new ListNode(4);
+        head = head.next;
+        head.next = new ListNode(3);
+        head = head.next;
+        head.next = new ListNode(2);
+        head = head.next;
+        head.next = new ListNode(1);
+        isPalindrome(head1);
+    }
 }
